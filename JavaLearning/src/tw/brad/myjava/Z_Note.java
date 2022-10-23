c4e47bebaefae5af7bc9827bd6831a2ba89f77d1
Administrator <Administrator@HT-20170208ZXCC>
2022-10-23 17:38:38 +0800

    20221023課堂教學註記
    註記各範例的教學內容

---
+++ JavaLearning/src/tw/brad/myjava/Z_Note.java
@@ -0,0 +1,53 @@
+package tw.brad.myjava;
+
+public class Z_Note {
+
+	public static void main(String[] args) {
+		// Just a note
+//		Brad01	空
+//		Brad02	空
+//		Brad03	空
+//		Brad04	Scanner概念
+//		Brad05	float概念
+//		Brad06	double概念
+//		Brad07	char概念
+//		Brad08	boolean概念
+//		Brad09	if...else概念
+//		Brad10	if...else概念
+//		Brad11	if...else概念
+//		Brad12	if...else概念
+//		Brad13	if...else實作-閏年判斷
+//		Brad14	if...else概念
+//		Brad15	switch概念
+//		Brad16	switch範例
+//		Brad17	for迴圈概念
+//		Brad18	Array陣列概念
+//		Brad19	骰子範例-switch實作
+//		Brad20	骰子範例-for實作
+//		Brad21	骰子作弊範例-for實作
+//		Brad22	Array多維陣列概念
+//		Brad23	do...while迴圈概念
+//		Brad24	計算1到N總和
+//		Brad25	Bike實作
+//		Brad26	JFrame方法
+//		Brad27	JFrame圖形介面實作
+//		Brad28	class繼承概念
+//		Brad29	a,b互換運算
+//		Brad30	String特性-不可變動
+//		Brad31	TWId建構式-實作
+//		Brad32	equals概念
+//		Brad33	class繼承概念
+//		Brad34	abstract抽象概念1
+//		Brad35	abstract抽象概念2
+//		Brad36	interface介面概念-繼承
+//		Brad37	interface介面概念-面積算法
+//		Brad38	迭代器-iterator
+//		Brad39	資料結構-Set介面-TreeSet
+//		Brad40	資料結構-List介面-LinkedList	
+//		Brad41	自動封裝
+//		Brad42	HashMap鍵值(key-value)映射
+		
+
+	}
+
+}
